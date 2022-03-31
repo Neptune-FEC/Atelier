796746ae796fdd5e5d8b16d7232da34ec8fb625a
acea68 <alancea01@gmail.com>
2022-03-31 17:18:58 -0500

    finished initial setup. React ready

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import "./main.css"
+
+
+const App = () => {
+  return <h1>Hello Neptune!</h1>
+};
+
+reactDOM.render(<App/>, document.getElementById("root"));
