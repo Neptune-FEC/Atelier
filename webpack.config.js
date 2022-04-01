687dd518103049682d3e2d89cd0d424fe00a6cd6
Tyler Fleshren <tfleshren@outlook.com>
2022-04-01 16:55:32 -0500

    adds dotenv module and .env.example file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "development",
@@ -31,4 +32,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  plugins: [
+    new Dotenv()
+  ]
 };
