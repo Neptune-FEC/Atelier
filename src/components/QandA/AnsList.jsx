938640e2f20dcaeac9764c6bd9f2bad835df8070
acea68 <alancea01@gmail.com>
2022-04-15 08:33:34 -0500

    last updates to qna

--- src/components/QandA/AnsList.jsx
+++ src/components/QandA/AnsList.jsx
@@ -11,36 +11,41 @@ class AnsList extends React.Component {
     const {
       ansList, numAns, updateAnsStateHelper, showingMoreAnswers,
     } = this.props;
-    // console.log('ansList from props in AnsList component: ', ansList);
+    // console.log('ansList from props in AnsList: ', ansList.length);
 
     return (
-      <div className="ansList">
-        {
-          showingMoreAnswers
-            ? (
-              <div className="ansListExpanded">
-                {ansList.map(((ans) => (
-                  <Answer
-                    answer={ans}
-                    key={ans.answer_id}
-                    updateAnsStateHelper={updateAnsStateHelper}
-                  />
+      <div>
+        {ansList.length === 0 ? (<div className="emptyAnsList">(No answers yet)</div>)
+          : (
+            <div className="ansList">
+              {
+              showingMoreAnswers
+                ? (
+                  <div className="ansListExpanded">
+                    {ansList.map(((ans) => (
+                      <Answer
+                        answer={ans}
+                        key={ans.answer_id}
+                        updateAnsStateHelper={updateAnsStateHelper}
+                      />
+                    )
+                    ))}
+                  </div>
+                ) : (
+                  <div className="ansListDefault">
+                    {ansList.slice(0, numAns).map(((ans) => (
+                      <Answer
+                        answer={ans}
+                        key={ans.answer_id}
+                        updateAnsStateHelper={updateAnsStateHelper}
+                      />
+                    )
+                    ))}
+                  </div>
                 )
-                ))}
-              </div>
-            ) : (
-              <div className="ansListDefault">
-                {ansList.slice(0, numAns).map(((ans) => (
-                  <Answer
-                    answer={ans}
-                    key={ans.answer_id}
-                    updateAnsStateHelper={updateAnsStateHelper}
-                  />
-                )
-                ))}
-              </div>
-            )
-        }
+            }
+            </div>
+          )}
       </div>
     );
   }
